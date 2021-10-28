602e53e004adf00f7c1f43976a3d11d5ece59f4f
Md. Jalal Uddin Chowdhury <JalalChowdhuryCSE1999@gmail.com>
2021-10-28 22:22:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const port = process.env.PORT || 5000 ;
+
+// middlewere
+app.use(cors());
+app.use(express.json());
+
+// connection for username and passs
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ug0c1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
+
+
+//main body
+
+async function run(){
+
+    try{
+        await client.connect();
+        const  databae = client.db('online_shop');
+        const productsCollection = databae.collection('products');
+
+        // GET PRODUCTS API
+        app.get('/products', async(req,res)=>{
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray(); 
+
+
+            res.send(products);
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+app.get('/' , (req,res)=>{
+    res.send("Ema john server is running");
+})
+
+app.listen(port, ()=>{
+    console.log("surver running at port ",port);
+})
